38c8552736641ad0a1c62f7ad4547cea37380e27
rlopessilva <ricardo.lopessilva@softeam.fr>
2019-01-25 15:01:01 +0100

    [CT-51][CT-55] WIP: socle projet et documentation de développement

--- user-skill-mgmt-api/src/main/java/fr/softeam/opus/userskillmgmt/resources/HelloResource.java
+++ user-skill-mgmt-api/src/main/java/fr/softeam/opus/userskillmgmt/resources/HelloResource.java
@@ -1,10 +1,16 @@
 package fr.softeam.opus.userskillmgmt.resources;
 
+import com.mdac.vertx.web.accesslogger.appender.printstream.impl.PrintStreamAppenderOptions;
+import com.mdac.vertx.web.accesslogger.impl.AccessLoggerOptions;
 import fr.softeam.opus.userskillmgmt.configuration.EventBusEnum;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
+import com.mdac.vertx.web.accesslogger.AccessLoggerHandler;
+import com.mdac.vertx.web.accesslogger.appender.logging.impl.LoggingAppenderOptions;
+
+import java.util.Arrays;
 
 public class HelloResource extends AbstractVerticle {
 
@@ -12,6 +18,18 @@ public class HelloResource extends AbstractVerticle {
     public void start(Future<Void> future) {
 
         Router router = Router.router(vertx);
+
+        router
+            .route()
+            .handler(AccessLoggerHandler.create(new AccessLoggerOptions().setPattern("%t %m %U %q %s %D"),
+                    Arrays.asList(
+//                            new PrintStreamAppenderOptions().setPrintStream(System.out),
+                            new LoggingAppenderOptions()
+                                    .setLoggerName("accesslog")
+                    )
+                    )
+            );
+
         router.get("/hello").handler(this::sayHello);
 
         vertx.createHttpServer()
--- user-skill-mgmt-api/src/main/java/module-info.java
+++ user-skill-mgmt-api/src/main/java/module-info.java
@@ -3,4 +3,6 @@ module user.skill.mgmt.application {
     requires vertx.web;
     requires slf4j.api;
     requires avro;
+    requires vertx.web.accesslog;
+    requires vertx.web.accesslog.logging.appender;
 }
