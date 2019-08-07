2aae3800b0664d6f13f5d1f4e1d640a9028184af
Leonardo ALARCON <e.leonardo.am@hotmail.com>
2019-08-07 14:45:43 +0200

    Ajout de la recherche de l'employé dans elasticsearch et une réponse 404

--- user-skill-mgmt-api/src/main/java/fr/softeam/opus/userskillmgmt/business/employee/EmployeeBlo.java
+++ user-skill-mgmt-api/src/main/java/fr/softeam/opus/userskillmgmt/business/employee/EmployeeBlo.java
@@ -11,6 +11,8 @@ import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.api.OperationRequest;
 import io.vertx.ext.web.api.OperationResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.client.RequestOptions;
 
 public class EmployeeBlo implements EmployeeService {
     private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeService.class);
@@ -24,10 +26,34 @@ public class EmployeeBlo implements EmployeeService {
     @Override
     public void getEmployeeById(String employeeId, OperationRequest context, Handler<AsyncResult<OperationResponse>> resultHandler) {
         LOGGER.debug("ENTERING getEmployeeById with param " + employeeId);
-        Employee employee = new Employee();
-        employee.setEmployeeId(employeeId);
+        // Creation de la requête elasticsearch avec les params : index, document et id
+        GetRequest getRequest = new GetRequest("skill_mgmt", "skills", employeeId);
+        // Exécution de la requête en async
+        client.getAsync(getRequest, RequestOptions.DEFAULT, ar -> {
+            // on traite l'employé si la requête s'est exécuté correctement
+            if (ar.succeeded()) {
+                // Si le resultat contient des informations on le renvoie en format json
+                if (ar.result().isExists()) {
+                    LOGGER.debug("result" + ar.result().getSourceAsString());
+                    Employee employee = new Employee(new JsonObject(ar.result().getSourceAsString()));
+                    resultHandler.handle(Future.succeededFuture(
+                            OperationResponse.completedWithJson(JsonObject.mapFrom(employee))
+                    ));
+                } else {
+                    // Si le résultat ne contient pas des informations en renvoie un 404
+                    LOGGER.warn("Employee with ID " + employeeId + " not found on index.");
+                    resultHandler.handle(Future.succeededFuture(
+                            new OperationResponse().setStatusCode(404)
+                                    .setStatusMessage("Employee with ID " + employeeId + " not found.")
+                    ));
+                }
+            } else {
+                LOGGER.error("An error occurred searching the employee.");
+                resultHandler.handle(Future.failedFuture("An error occurred searching the employee. " +
+                        "Please try again later... Or never, whatever."));
+            }
+        });
 
-        resultHandler.handle(Future.succeededFuture(OperationResponse.completedWithJson(JsonObject.mapFrom(employee))));
         LOGGER.debug("EXITING getEmployeeById with param " + employeeId);
     }
 }
