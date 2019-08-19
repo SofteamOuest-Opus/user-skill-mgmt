edbb810b108779392a772c15b6f377e54ca51478
Leonardo ALARCON <e.leonardo.am@hotmail.com>
2019-08-19 16:01:12 +0200

    Modification de l'arborescence du projet

---
+++ user-skill-mgmt-client/src/components/app/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import EmployeeSkills from '../employee/employee-skills-container';
+
+class App extends Component {
+  render() {
+    return (
+      <EmployeeSkills />
+    );
+  }
+}
+
+export default App;
