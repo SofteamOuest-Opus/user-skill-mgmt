17be1a3658f7021f07ec1df5d40e5e3f83ddcd3a
Leonardo ALARCON <e.leonardo.am@hotmail.com>
2019-08-19 16:02:34 +0200

    Création du composant container d'un employé

---
+++ user-skill-mgmt-client/src/components/employee/employee-skills-container.tsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Constants from '../../utils/Constants';
+import Employee from '../../utils/interfaces/employee';
+
+interface EmployeeSkillsState {
+    isLoading: boolean,
+    employee?: Employee,
+    error: any
+}
+
+class EmployeeSkills extends Component<{}, EmployeeSkillsState> {
+    constructor(props:  {}){
+        super(props);
+        this.state = {
+            isLoading: true,
+            error: null
+        }
+    }
+
+    componentDidMount() : void {
+        fetch(Constants.API + Constants.EMPLOYEE_API.replace("{employeeId}", "1"))
+            .then(response => response.json())
+            .then(responseJson => {
+                this.setState({isLoading: false, employee: responseJson, error: null});
+            })
+            .catch(error => {
+                console.error(error);
+                this.setState({isLoading: false, error: error});
+            });
+    }
+
+
+    render(){
+        let info = <p>Error fetching the employee info</p>
+        if(this.state.isLoading){
+            info = <p>Loading...</p>
+        } else {
+            if(this.state.error){
+                info = <p>An error ocurred : {this.state.error}</p>
+            } else if(this.state.employee){
+                let employee : Employee = this.state.employee;
+                info = <p>Employee {employee.employeeId} </p>;
+            }
+        }
+        return (
+            <div>{info}</div>
+        );
+    }
+}
+
+export default EmployeeSkills;
