c273d97da6fc8075a0671bd2ba19065aa40a6ff1
Leonardo ALARCON <e.leonardo.am@hotmail.com>
2019-08-20 11:48:30 +0200

    Modification du nom du fichier employee-container

--- user-skill-mgmt-client/src/components/app/App.tsx
+++ user-skill-mgmt-client/src/components/app/App.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import EmployeeSkills from '../employee/employee-skills-container';
+import EmployeeSkills from '../employee/employee-container';
 
 class App extends Component {
   render() {
