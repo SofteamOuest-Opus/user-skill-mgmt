e0830fae65d365ba15509d2ee477256be938e2e1
Leonardo ALARCON <e.leonardo.am@hotmail.com>
2019-08-20 11:46:37 +0200

    Modification de l'emplacement de la méthode de rendre des skills

--- user-skill-mgmt-client/src/components/employee/employee-skill-view.tsx
+++ user-skill-mgmt-client/src/components/employee/employee-skill-view.tsx
@@ -4,6 +4,23 @@ import Skill from "../../utils/interfaces/skill";
 
 
 const EmployeeSkillView: SFC<{employee : Employee}> = (props) => {
+    // We create card displaying the informations of each skill.
+    const renderSkills = (skills : Skill[]) => {
+        return skills.map((skill) => {
+            return (
+                <div className="card col-4" key={skill.id}>
+                    <div className="card-body">
+                        <h5 className="card-title">{skill.label}</h5>
+                        <h6 className="card-subtitle mb-2 text-muted">{skill.level}</h6>
+                        <div className="card-text">
+                            <p><small>Category : </small>{skill.category.label}</p>
+                        </div>
+                    </div>
+                </div>
+            )
+        });
+    }
+    
     return (
         <div>
             <h5>Skills</h5>
@@ -12,23 +29,6 @@ const EmployeeSkillView: SFC<{employee : Employee}> = (props) => {
     );
 }
 
-/**
- * We create card displaying the informations of each skill.
- */
-const renderSkills = (skills : Skill[]) => {
-    return skills.map((skill) => {
-        return (
-            <div className="card col-4" key={skill.id}>
-                <div className="card-body">
-                    <h5 className="card-title">{skill.label}</h5>
-                    <h6 className="card-subtitle mb-2 text-muted">{skill.level}</h6>
-                    <div className="card-text">
-                        <p><small>Category : </small>{skill.category.label}</p>
-                    </div>
-                </div>
-            </div>
-        )
-    });
-}
+
 
 export default EmployeeSkillView;
