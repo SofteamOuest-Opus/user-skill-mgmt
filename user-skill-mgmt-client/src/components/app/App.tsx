05bcdd1e62cf34b91e024caf6881c462485dd28c
Leonardo ALARCON <e.leonardo.am@hotmail.com>
2019-08-20 16:40:55 +0200

    Suppression imports inécessaires

--- user-skill-mgmt-client/src/components/app/App.tsx
+++ user-skill-mgmt-client/src/components/app/App.tsx
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import EmployeeSkills from '../employee/employee-container';
 import Routes from '../../router';
 import { Link } from 'react-router-dom';
 
